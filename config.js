e376f4dda839032015dd130c75fb5728aaeb1ce6
airrishlucky <140961211+airrishlucky@users.noreply.github.com>
2026-08-29 10:23:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.PH10_ACCESS_CONFIG = {
     systemName: "PH10 Access Control",
-    toolId: "WFAC",
+
     usersUrl:
         "https://airrishlucky.github.io/ph10_buttons_malakas/ph10-access/users.json"
 };
